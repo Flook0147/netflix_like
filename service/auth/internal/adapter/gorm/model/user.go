2c3d0e79abb0bd00c2b389fa5c80d722ced12140
Flook0147 <flook.akkharaphon@gmail.com>
2026-01-25 18:15:48 +0700

    add: add auth service

--- service/auth/internal/adapter/gorm/model/user.go
+++
@@ -1,9 +0,0 @@
-// internal/adapter/gorm/model/user.go
-package model
-
-type User struct {
-	ID       string `gorm:"primaryKey"`
-	Username string `gorm:"uniqueIndex;not null"`
-	Email    string `gorm:"uniqueIndex;not null"`
-	Password string `gorm:"not null"`
-}
